9fceebb5274224c4484f68f31029b8d8e8c27420
YanhuiJessica <137126578@qq.com>
2022-06-12 15:50:26 +0800

    加解密代码复用

--- RC4/rc4.py
+++ RC4/rc4.py
@@ -27,61 +27,42 @@ def PRGA(S):
         k = S[(S[i] + S[j]) % 256]
         yield k # 返回生成器，当调用函数时并不运行函数体内的代码，每次调用 __next__() 执行一次循环体
 
-def encrypt(key: str, plaintext: str, hexstr: bool = False) -> str:
-    '''
-    `key` - 用于加密的密钥
+def crypt(op) -> None:
+    def wrapper(key: str, text: str, hexstr: bool = False) -> str:
+        '''
+        `key` - 用于加/解密的密钥
 
-    `plaintext` - 需要加密的明文
+        `text` - 需要加密/解密的文本
 
-    `hexstr` - 标记明文是否是十六进制字符串
+        `hexstr` - 标记文本是否是十六进制字符串
 
-    返回密文十六进制字符串
-    '''
-    # 将字符转换为字节
-    key = key.encode('utf-8')
-    # 依据明文类型采用不同方式
-    if hexstr: plain = bytes.fromhex(plaintext)
-    else: plain = plaintext.encode('utf-8')
+        返回密文/明文十六进制字符串
+        '''
+        key = key.encode('utf-8')
 
-    keystream = PRGA(KSA(key))  # 取得密钥流
+        if hexstr: text = bytes.fromhex(text)
+        else: text = text.encode('utf-8')
 
-    cipher = []
-    for c in plain:
-        val = '%02x' % (c ^ keystream.__next__())  # 异或并转化为 hex 值
-        cipher.append(val)
+        keystream = PRGA(KSA(key))  # 取得密钥流
 
+        cipher = []
+        for c in text:
+            val = '%02x' % (c ^ keystream.__next__())  # 异或并转化为 hex 值
+            cipher.append(val)
+
+        op()
+        print_result(''.join(cipher))
+
+        return ''.join(cipher)
+    return wrapper
+
+@crypt
+def encrypt() -> str:
     print('\nEncrypted data:')
-    print_result(''.join(cipher))
 
-    return ''.join(cipher)
-
-def decrypt(key: str, ciphertext: str, hexstr: bool = False) -> str:
-    '''
-    `key` - 用于解密的密钥
-
-    `ciphertext` - 需要解密的密文
-
-    `hexstr` - 标记密文是否是十六进制字符串
-
-    返回明文十六进制字符串
-    '''
-    # 将字符转化为字节
-    key = key.encode('utf-8')
-    # 依据密文类型采用不同方式
-    if hexstr: cipher = bytes.fromhex(ciphertext)
-    else: cipher = ciphertext.encode('utf-8')
-
-    keystream = PRGA(KSA(key))  # 取得密钥流
-
-    plain = []
-    for c in cipher:
-        val = '%02x' % (c ^ keystream.__next__())  # 异或并转化为 hex 值
-        plain.append(val)
-
+@crypt
+def decrypt() -> str:
     print('\nDecrypted data:')
-    print_result(''.join(plain))
-
-    return ''.join(plain)
 
 def print_result(text: str) -> None:
     print('HEX: 0x' + text)
